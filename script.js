ada0f4046124bfb7a2366a0669fb485080df0751
Shenal Tissera <tisserashenal@gmail.com>
2025-02-04 01:43:01 -0500

    adding second changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // URL
-const url = 'https://raw.githubusercontent.com/RahulMuk/CodingTheNews-2025/refs/heads/main/_datasets/artist_data.csv?token=GHSAT0AAAAAAC5YIWRQSBFFHFFGPDLUIKQSZ5AOM3A'
+const url = 'https://raw.githubusercontent.com/RahulMuk/CodingTheNews-2025/refs/heads/main/_datasets/artist_data.csv?token=GHSAT0AAAAAAC5YIWRQCGWC4A5SIYUKOZMIZ5BX37Q'
 
 // Create a function to parse the CSV into an array
 function parseCSV(data) { 
     const regex = /,(?=(?:[^"]*"[^"]*")*[^"]*$)/; // Sets regex rule in order to split based on commas but not commas inside double-quoted substrings
-   // console.log(data)
+    //console.log(data)
     const rows = data.trim().split('\n'); // Splits the csv into three rows
-   // console.log(rows)
+    //console.log(rows)
     const headers = rows[0].split(','); // Splits the first rows into header names
     //console.log(headers)
 
@@ -17,19 +17,27 @@ function parseCSV(data) {
            // return cell.replace(/^"(.+)"$/, '$1');
 
         const vals = row.split(regex).map(r => {
-            console.log(r)
+            //console.log(r)
             return r.replace(/^"(.+)"$/, '$1');
         }); // Split up all the info
-        console.log(vals)
+        //console.log(vals)
         const obj = {}; // Init the container to be created containing the relevant info for each of the artists
         headers.forEach((header, i) => {
             //console.log(vals[i])
             obj[header.trim()] = vals[i]?.trim(); // ? in case no value is there so as not to break the code, filling the artist with the proper info
         });
-       // console.log(obj)
+        if(obj.name === "Dua Lipa"){
+            obj.path = "img/dua.webp"
+        } else if(obj.name === "Kacey Musgraves"){ // Adding the img paths
+            obj.path = "img/kacey.webp"
+        } else if(obj.name === "Kendrick Lamar"){
+            obj.path = "img/kungfukenny.webp"
+        }
+        obj.totalWeeks = Number(obj.song_1_weeks_on_top_100) + Number(obj.song_2_weeks_on_top_100) + Number(obj.song_3_weeks_on_top_100) + Number(obj.song_4_weeks_on_top_100) + Number(obj.song_5_weeks_on_top_100)
+        console.log(obj)
         return obj; // Fill the results container with the artist 
     });
-    console.log(res)
+    //console.log(res)
     return res; // Contains the three artists and their info
 }
 
@@ -48,16 +56,24 @@ function populateHTML(data){
     container.innerHTML = ''; // Clear the div
 
     // Go through each artist and make a card
-    data.forEach(artist => { // put , i and wrap in a ()
+    data.forEach((artist, i) => { // put , i and wrap in a ()
         const artDiv = document.createElement('div');
         artDiv.classList.add('artist');
 
         // Populate div for each artist
         artDiv.innerHTML = `
-            <h1>${artist['name']}</h1>
-            <h2>Hometown: ${artist['hometown']}</h2>
-            <h3>Genre ${artist['primary_genre']}</h3>
-            <p><strong>Albums:</strong> ${artist['albums_released']}</p>
+            <div class="img-container">
+                <img src="${artist['path']}" alt="${artist['name']}" />
+            </div>
+            <div class="info">
+                <h1>${artist['name']}</h1>
+                <h2>Hometown: ${artist['hometown']}</h2>
+                <h3>Genre: ${artist['primary_genre']}</h3>
+                <h3>Albums: ${artist['albums_released']}</h3>
+                <h3>Highest Peaks on Billboard: ${artist['song_1_highest_peak']}, ${artist['song_2_highest_peak']}, ${artist['song_3_highest_peak']}, ${artist['song_4_highest_peak']}, ${artist['song_5_highest_peak']}</h3>
+                <h3>Total Weeks on Top 100: ${artist['totalWeeks']}</h3>
+            </div>
+            
         `;
         container.appendChild(artDiv) // Append the artist div to the container
     })
